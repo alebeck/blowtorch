0d41566db771687ceec7cbe2451dfe9adb614dda
Alexander Becker <alexander.becker@tum.de>
2021-03-03 17:41:22 +0100

    0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aurora',
-    version='0.0.2',
+    version=0.2,
     description='Pytorch trainer',
     author='Alexander Becker',
     python_requires='>= 3.6',
